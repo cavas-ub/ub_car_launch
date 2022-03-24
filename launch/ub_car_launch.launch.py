3eab86994f51f025da1ac5a67ac4613c46775b4e
Zhihao Ruan <ruanzh@seas.upenn.edu>
2022-03-23 23:51:46 -0400

    format

--- launch/ub_car_launch.launch.py
+++ launch/ub_car_launch.launch.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """
 
 """
@@ -28,23 +27,19 @@ from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 def get_share_file_path(package_name, file_name):
     return os.path.join(get_package_share_directory(package_name), file_name)
 
+
 def generate_launch_description():
     """Launch stuff for UB car"""
 
-    dataspeed_dbw = IncludeLaunchDescription(
-        XMLLaunchDescriptionSource(
-            get_share_file_path('dbw_ford_can', 'launch/dbw.launch.xml')
-        ),
-        launch_arguments={
-            "ulc": "false"
-        }.items()
-    )
+    dataspeed_dbw = IncludeLaunchDescription(XMLLaunchDescriptionSource(
+        get_share_file_path('dbw_ford_can', 'launch/dbw.launch.xml')),
+                                             launch_arguments={"ulc": "false"
+                                                               }.items())
 
     dataspeed_interface = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            get_share_file_path('dataspeed_ford_interface', 'launch/dataspeed_ford_interface.launch.py')
-        ),
-        launch_arguments={}.items()
-    )
+            get_share_file_path('dataspeed_ford_interface',
+                                'launch/dataspeed_ford_interface.launch.py')),
+        launch_arguments={}.items())
 
     return launch.LaunchDescription([dataspeed_dbw, dataspeed_interface])
